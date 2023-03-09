90c30518dc6cc3e3ad16ce28432b928b1895bf47
kodakr <111961380+kodakr@users.noreply.github.com>
2023-03-09 20:29:20 +0100

    gfdg

--- Masterchef_imp.sol
+++ Masterchef_imp.sol
@@ -1,1 +1,6 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
